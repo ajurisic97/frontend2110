a4d97e47cda5f5a356048b84be4efe430657a041
Andelo Jurisic <ajurisic@pmfst.hr>
2020-10-21 17:19:19 +0200

    Herou backend

--- src/services/poruke.js
+++ src/services/poruke.js
@@ -1,7 +1,8 @@
 	
 import axios from 'axios'
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+//const osnovniUrl = 'http://localhost:3001/api/poruke'
 //const osnovniUrl = '/api/poruke'
+const osnovniUrl = `https://aj04-poruke.herokuapp.com/api/poruke`
 
 const dohvatiSve = () => {   
     const promise = axios.get(osnovniUrl);
